5c2efc432eea707dc1435cb8a2720de71acd2573
elahe <elahesdi1378@gmail.com>
2021-01-31 23:27:09 +0330

    SignUpGUI class constructor

--- src/GUI/SignUpGUI.java
+++ src/GUI/SignUpGUI.java
@@ -10,7 +10,24 @@ public class SignUpGUI extends JFrame {
 
     public  SignUpGUI(){
       
-
+ jFrame=new JFrame("Info");
+        jFrame.setBackground(Color.LIGHT_GRAY);
+        jFrame.setSize(new Dimension(700,700));
+        jFrame.setResizable(false);
+        jFrame.setLocation(600,150);
+        JPanel jPanel1=new JPanel(new GridLayout(7,2));
+        jPanel1.setPreferredSize(new Dimension(690,790));
+        jPanel1.setBackground(Color.DARK_GRAY);
+        JLabel jLabel=new JLabel(" Name :");
+        Font font=new Font("Verdana",Font.BOLD,12);
+        jLabel.setFont(font);
+        jLabel.setForeground(Color.white);
+        jPanel1.add(jLabel);
+        JLabel jLabel1=new JLabel(" Value :");
+        jLabel.setBackground(Color.LIGHT_GRAY);
+        jLabel1.setFont(font);
+        jLabel1.setForeground(Color.white);
+        jPanel1.add(jLabel1);
 
     }
 
