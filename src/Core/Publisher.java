dbce6346d1b30e57c8e6ed83839c80bb454d3704
9733020 <9733020@ceit.local>
2021-01-31 17:58:18 +0330

    Write Publisher.java

--- src/Core/Publisher.java
+++ src/Core/Publisher.java
@@ -1,5 +1,27 @@
 package Core;
 
+import java.util.ArrayList;
+
 public class Publisher
 {
+    private String name;
+    private ArrayList<Book> books;
+
+    public Publisher(String name)
+    {
+        this.name = name;
+        books = new ArrayList<>();
+    }
+
+
+    public void addBook(Book book)
+    {
+
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
 }
