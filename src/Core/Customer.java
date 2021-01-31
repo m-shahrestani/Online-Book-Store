f14a7b803cca292fe4a84db9d64e169c710c95f3
9733020 <9733020@ceit.local>
2021-01-31 17:58:36 +0330

    Write Customer.java

--- src/Core/Customer.java
+++ src/Core/Customer.java
@@ -1,6 +1,19 @@
 package Core;
 
+import java.util.ArrayList;
+
 public class Customer
 {
+    String name;
+    ArrayList<Book> cart;
 
+    public Customer(String name)
+    {
+        this.name = name;
+    }
+
+    public void addBookToCart()
+    {
+
+    }
 }
