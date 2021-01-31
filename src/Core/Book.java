200fa8f5a90daecdc5e08ef24e3122c581e71bd1
9733020 <9733020@ceit.local>
2021-01-31 17:58:03 +0330

    Write Book.java

--- src/Core/Book.java
+++ src/Core/Book.java
@@ -2,4 +2,14 @@ package Core;
 
 public class Book
 {
+    private String name;
+    private String author;
+    private String edition;
+
+    public Book(String name, String author, String edition)
+    {
+        this.name = name;
+        this.author = author;
+        this.edition = edition;
+    }
 }
