928f6a0bbc525c0d426907d545e12a1ff5d3955b
9725040 <9725040@ceit.local>
2021-01-31 20:49:30 +0330

    Add book Class

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,5 @@
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello");
-        System.out.println();
-	//maryam
+    public static void main(String[] args){
     }
 }
