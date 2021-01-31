1f6910b47d3868759ca4213aea47f2b90688f83c
9733020 <9733020@ceit.local>
2021-01-31 17:59:08 +0330

    Write CustomerGUI.java

--- src/GUI/CustomerGUI.java
+++ src/GUI/CustomerGUI.java
@@ -1,15 +1,30 @@
 package GUI;
 
+import Core.Customer;
 import Core.Server;
+
+import javax.swing.*;
 
 public class CustomerGUI
 {
     Server server;
+    Customer customer;
 
-    public CustomerGUI(Server server)
+    public CustomerGUI(Server server, Customer customer)
     {
         this.server = server;
+        this.customer = customer;
+
+        showFrame();
     }
 
+    private void showFrame()
+    {
+        JFrame frame = new JFrame("Customer Panel");
+
+        if (customer != null)
+
+        frame.setVisible(true);
+    }
 
 }
