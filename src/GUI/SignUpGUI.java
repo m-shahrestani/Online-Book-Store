f3a3f64ee045a3b484ab78fd0fd71a0b82aad530
elahe <elahesdi1378@gmail.com>
2021-01-31 23:25:25 +0330

    SignUpGUI class init

---
+++ src/GUI/SignUpGUI.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class SignUpGUI extends JFrame {
+    private JButton done;
+    private JTextField name;
+    private JTextField address;
+    private JTextField phoneNumber;
+    private JFrame jFrame;
+
+    public  SignUpGUI(){
+      
+
+
+    }
+
+    
+
+}
