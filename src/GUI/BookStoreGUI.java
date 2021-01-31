437d28c7607d19748102fea80791e17b65aa069e
m-shahrestani <9728054@ceit.local>
2021-01-31 22:07:07 +0330

    Add frame

---
+++ src/GUI/BookStoreGUI.java
@@ -0,0 +1,53 @@
+package GUI;
+import javax.swing.*;
+import java.awt.*;
+
+public class BookStoreGUI {
+    //singleton instance
+    private static BookStoreGUI INSTANCE = null;
+    //frame
+    private static JFrame frame;
+
+    private BookStoreGUI(){
+        frame = new JFrame();
+        //look and feel
+        try {
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Windows".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception ignored) {}
+
+        makeFrame();
+
+        frame.setVisible(true);
+    }
+
+    /**
+     * Create a new TankTroubleGUI with singleton.
+     *
+     * @return INSTANCE.
+     */
+    public static BookStoreGUI getInstance() {
+        if (INSTANCE == null)
+            INSTANCE = new BookStoreGUI();
+        return INSTANCE;
+    }
+
+    /**
+     * make frame
+     *
+     * The method makes frame for TankTroubleGUI.
+     */
+    private void makeFrame() {
+        frame.setTitle("Online Book store");
+        frame.setSize(740, 530);
+        frame.setLocation(300, 200);
+        frame.setBackground(Color.white);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
+import GUI.BookStoreGUI;
+
 public class Main {
 
     public static void main(String[] args) {
+        BookStoreGUI bookStoreGUI1 = BookStoreGUI.getInstance();
         System.out.println("Hello");
         System.out.println();
     }
+
 }
