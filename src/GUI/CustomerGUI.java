a8d73791db3812a765522ba15cada105d6ac0e59
9733020 <9733020@ceit.local>
2021-01-31 18:36:29 +0330

    Add components for CustomerGUI.java frame and Add AddToCartActionListener class for buttons

--- src/GUI/CustomerGUI.java
+++ src/GUI/CustomerGUI.java
@@ -1,9 +1,12 @@
 package GUI;
 
+import Core.Book;
 import Core.Customer;
 import Core.Server;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class CustomerGUI
 {
@@ -22,9 +25,46 @@ public class CustomerGUI
     {
         JFrame frame = new JFrame("Customer Panel");
 
-        if (customer != null)
+        JPanel databaseBooksPanel = new JPanel();
+        JScrollPane databaseBooksScroll = new JScrollPane(databaseBooksPanel);
+        JPanel cartPanel = new JPanel();
+        JScrollPane cartScroll =  new JScrollPane(cartPanel);
 
+        updateDatabaseBooksPanel(databaseBooksPanel);
+
+        frame.add(cartPanel);
+        frame.add(databaseBooksPanel);
         frame.setVisible(true);
     }
 
+    public void updateDatabaseBooksPanel(JPanel databaseBooksPanel)
+    {
+
+    }
+
+    public void updateCartPanel(JPanel cartPanel)
+    {
+
+    }
+}
+
+class AddToCartActionListener implements ActionListener
+{
+    Customer customer;
+    Server server;
+    Book book;
+
+    public AddToCartActionListener(Server server, Customer customer, Book book)
+    {
+        this.customer = customer;
+        this.server = server;
+        this.book = book;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+        server.addBook(book);
+        customer.addBookToCart(book);
+    }
 }
