6428dca86a9b7bdf87e1be77c819ef89276d1fb8
9725040 <9725040@ceit.local>
2021-02-02 19:38:03 +0330

    Add labels and fields in PublisherAddBookPanel class

--- src/GUI/PublisherAddBookPanel.java
+++ src/GUI/PublisherAddBookPanel.java
@@ -1,6 +1,7 @@
 package GUI;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class PublisherAddBookPanel extends JPanel
 {
@@ -11,10 +12,55 @@ public class PublisherAddBookPanel extends JPanel
     private JTextField editionTextField;
     private JTextField priceTextField;
     private JTextField numberTextField;
-    private JTextField summeryTextField;
+    private JTextArea summeryTextArea;
 
     public PublisherAddBookPanel()
     {
-
+        setLayout(new GridBagLayout());
+        GridBagConstraints constr = new GridBagConstraints();
+        constr.insets = new Insets(5, 5, 5, 5);
+        constr.anchor = GridBagConstraints.WEST;
+        nameTextField = new JTextField(20);
+        authorTextField = new JTextField(20);
+        ageRateTextField = new JTextField(20);
+        subjectTextField = new JTextField(20);
+        editionTextField = new JTextField(20);
+        priceTextField = new JTextField(20);
+        numberTextField = new JTextField(20);
+        summeryTextArea = new JTextArea(5,20);
+        addAllLabels(constr, this);
+        addAllTextFields(constr, this);
+        constr.gridx=1; constr.gridy=7;
+        add(summeryTextArea, constr);
+    }
+    public void addLabel(String labelDescription, GridBagConstraints constr, JPanel panel, int gridY){
+        JLabel label = new JLabel(labelDescription);
+        constr.gridx = 0;
+        constr.gridy = gridY;
+        panel.add(label, constr);
+    }
+    public void addTextField(JTextField textField, GridBagConstraints constr, JPanel panel, int gridY){
+        constr.gridx = 1;
+        constr.gridy = gridY;
+        panel.add(textField, constr);
+    }
+    public void addAllLabels(GridBagConstraints constr, JPanel panel){
+        addLabel("Name", constr, panel, 0);
+        addLabel("Author", constr, panel, 1);
+        addLabel("AgeRate", constr, panel, 2);
+        addLabel("Subject", constr, panel, 3);
+        addLabel("Edition", constr, panel, 4);
+        addLabel("Price", constr, panel, 5);
+        addLabel("Number", constr, panel, 6);
+        addLabel("Summery", constr, panel, 7);
+    }
+    public void addAllTextFields(GridBagConstraints constr, JPanel panel){
+        addTextField(nameTextField, constr, panel, 0);
+        addTextField(authorTextField, constr, panel, 1);
+        addTextField(ageRateTextField, constr, panel, 2);
+        addTextField(subjectTextField, constr, panel, 3);
+        addTextField(editionTextField, constr, panel, 4);
+        addTextField(priceTextField, constr, panel, 5);
+        addTextField(numberTextField, constr, panel, 6);
     }
 }
